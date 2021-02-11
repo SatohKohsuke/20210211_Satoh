a487cf7b56a9d8098a3bce8d15cb759ae5fd63ca
SatohKohsuke <76554360+SatohKohsuke@users.noreply.github.com>
2021-02-11 22:43:16 +0900

    Add files via upload

---
+++ src/views/Home.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="post-number">
+        <input v-model="text" placeholder="郵便番号(ハイフンなし)">
+        <button @click="search">検索</button>
+        <p class="address">住所：{{ streetaddress }}</p>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+
+
+export default {
+  data() {
+    return {
+      apikey: "FntgQMfS0Cx9pZ8A8aSpTJksncx853dmuESJMdO",
+      streetaddress: "",
+    };
+  },
+  methods: {
+    search() {
+      let url = "https://apis.postcode-jp.com/api/v3/postcodes/"+this.text+"?apikey="+this.apikey;
+      axios.get(url).then((res) => {
+      this.streetaddress = res.data.allAddress;
+      });
+    },
+  },
+}
+</script>
